805e5567b0e6af2874edce0d012fb27b5249b5a1
GabrielRomanoDev <125510299+GabrielRomanoDev@users.noreply.github.com>
2024-07-10 17:09:06 -0300

    Time: 18 ms (41.66%), Space: 16.7 MB (22.79%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.swift
@@ -0,0 +1,33 @@
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+    
+    let dict: [String:Int] = ["I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000]
+    let array: [String] = s.map{ String($0) }
+    var n = s.count - 1
+    var sum: Int = 0
+    
+    repeat {
+        
+        let actualNumber: Int = dict[array[n]] ?? 0
+        
+        if n>0{
+            let prevNumber: Int = dict[array[n-1]] ?? 0
+            
+            if actualNumber <= prevNumber{
+                sum += actualNumber
+                n -= 1
+            } else{
+                sum = sum + actualNumber - prevNumber
+                n -= 2
+            }
+        } else {
+            sum += actualNumber
+            return sum
+        }
+        
+    } while(n>=0)
+    
+    return sum
+}
+
+}
